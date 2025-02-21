284281ec53938fd7b532798f69f37e55ad59cce4
dsura22 <dsura22@freeuni.edu.ge>
2025-02-21 17:59:23 +0400

    Added Implementation of Buy-N-Get-N Sqlite Store

--- finalproject/store/distributor.py
+++ finalproject/store/distributor.py
@@ -1,11 +1,15 @@
 import sqlite3
 from typing import Protocol
 
+from finalproject.store.buy_n_get_n import BuyNGetNSQLiteStore, BuyNGetNStore
 from finalproject.store.product import ProductSQLiteStore, ProductStore
 
 
 class StoreDistributor(Protocol):
     def products(self) -> ProductStore:
+        pass
+
+    def buy_n_get_n(self) -> BuyNGetNStore:
         pass
 
     def destruct(self) -> None:
@@ -21,11 +25,15 @@ class SQLiteStoreDistributor:
 
         self._products = ProductSQLiteStore(connection)
         # Add new stores here
+        self._buy_n_get_n = BuyNGetNSQLiteStore(connection)
 
         self._connection = connection
 
     def products(self) -> ProductStore:
         return self._products
+
+    def buy_n_get_n(self) -> BuyNGetNStore:
+        return self._buy_n_get_n
 
     def destruct(self) -> None:
         self._connection.close()
